9c877f8d13b27ac35f3c7c8ec05d9cef817c80eb
Minh Le <53259342+minhleduc@users.noreply.github.com>
2020-08-20 10:19:53 +0700

    Add project files.

---
+++ EShopWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EShopWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
